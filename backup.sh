d7fe3d8632abd1400766d03e2bf0f1e96ec8eb68
Van Dravik <jan@vandravik.com>
2019-12-18 11:28:15 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -12,3 +12,6 @@ sudo -u www-data php occ maintenance:mode --on
 # copy the full nextcloud path
 rsync -Aavx "$RSYNCSRC/" $RSYNCDEST/nextcloud-dirbkp_`date +"%Y%m%d"`/
 
+# dump bdd
+
+mysqldump --single-transaction -h [server] -u [username] -p[password] [db_name] > nextcloud-sqlbkp_`date +"%Y%m%d"`.bak
