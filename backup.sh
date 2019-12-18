ab0cd2f9bbdd90ef71c62186265a3909c67300da
Van Dravik <jan@vandravik.com>
2019-12-18 15:21:40 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -15,3 +15,11 @@ rsync -Aavx "$RSYNCSRC/" $RSYNCDEST/nextcloud-dirbkp_`date +"%Y%m%d"`/
 # dump bdd
 
 mysqldump --single-transaction -h [server] -u [username] -p[password] [db_name] > $RSYNCDEST/nextcloud-sqlbkp_`date +"%Y%m%d"`.bak
+
+
+# restore
+# rsync -Aax nextcloud-dirbkp/ nextcloud/
+# mysql -h [server] -u [username] -p[password] -e "DROP DATABASE nextcloud"
+# mysql -h [server] -u [username] -p[password] -e "CREATE DATABASE nextcloud"
+# ou mysql -h [server] -u [username] -p[password] -e "CREATE DATABASE nextcloud CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci"
+# mysql -h [server] -u [username] -p[password] [db_name] < nextcloud-sqlbkp.bak
